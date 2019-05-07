01a3f27f132ac013083821a3c49a9da2b2fb71a8
JLee <jlee@accent-technologies.com>
2019-05-07 14:33:18 -0400

    Initial Commit

---
+++ GradientLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientLayer
+//
+//  Created by Jae Lee on 5/7/19.
+//  Copyright © 2019 Jae Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
